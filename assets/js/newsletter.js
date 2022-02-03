1999691eddbbb229dd75e3f824288d51c3c4c72a
Michał <michal.jablonski097@gmail.com>
2022-02-03 19:08:02 +0100

    newsletter iframe

---
+++ assets/js/newsletter.js
@@ -0,0 +1,2 @@
+var fmFCF2awthms2vk=function(e){if(e.data.type==='resize'){document.getElementById('fm-fc-f-2awthms2vk').style.minHeight=e.data.size+'px'}};window.addEventListener?addEventListener('message',fmFCF2awthms2vk,!1):attachEvent('onmessage',fmFCF2awthms2vk);
+
