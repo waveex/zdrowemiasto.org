f077a07c512ad7b2aa323e2e07435e050ebd4902
waveex <michal.jablonski097@gmail.com>
2023-01-30 21:31:28 +0100

    two new pojects tabs and google translator fix

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,11 +2,12 @@ const modal = document.querySelector(".modal");
 
 const browserLanguage =
   window.navigator.userLanguage || window.navigator.language;
+const isoCode = browserLanguage.substring(0, browserLanguage.length - 3);
 const htmlTag = document.documentElement;
 const modalButtons = document.querySelector(".modal__buttons");
 const buildModalButtons = () => {
   modalButtons.innerHTML = `<button class="modal__button modal__button--decline  modal__button--decline-js parallax__button">Decline</button>
-  <a class="modal__button modal__button--aprove parallax__button" href="https://zdrowemiasto-org.translate.goog/?_x_tr_sl=pl&_x_tr_tl=${browserLanguage}&_x_tr_hl=${browserLanguage}&_x_tr_pto=sc" rel="noindex nofollow">Aprove</a>`;
+  <a class="modal__button modal__button--aprove parallax__button" href="https://zdrowemiasto-org.translate.goog/?_x_tr_sl=pl&_x_tr_tl=${isoCode}&_x_tr_hl=${isoCode}&_x_tr_pto=sc" rel="noindex nofollow">Aprove</a>`;
 };
 const modalCloser = (e) => {
   const modalDecline = document.querySelector(".modal__button--decline-js");
@@ -14,13 +15,14 @@ const modalCloser = (e) => {
     modal.classList.add("hide");
   });
 };
-setTimeout( () => {
-if (browserLanguage !== "pl") {
-  const isTranslated = htmlTag.classList.contains("translated-ltr");
-  if (!isTranslated) {
-    modal.classList.add("showModal");
-    buildModalButtons();
-    modalCloser();
+setTimeout(() => {
+  console.log(isoCode);
+  if (browserLanguage !== "pl-Pl") {
+    const isTranslated = htmlTag.classList.contains("translated-ltr");
+    if (!isTranslated) {
+      modal.classList.add("showModal");
+      buildModalButtons();
+      modalCloser();
     }
-}
-}, 500)
+  }
+}, 500);
